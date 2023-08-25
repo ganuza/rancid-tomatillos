148a569e91d1e8c71116aff26464e177c359ccda
Adrian Zabolitzki <adrianzab@gmail.com>
2023-08-25 17:17:30 -0600

    Add showIndividualMovie function

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import Movies from './MovieBox';
 
 function App() {
   const [movies, setMovies] = useState(movieData.movies);
+  const [individualMovie, setIndividualMovie] = useState(null)
+
+  function showIndividualMovie(id) {
+    const individualMovie = movies.find(movie => movie.id === id)
+    setIndividualMovie(individualMovie)
+  }
 
   return (
     <div>
       {/* <h1>{movieData.movies[0].title}</h1>
       <img src={movieData.movies[0].poster_path} /> */}
-      <Movies movies={movies}/>
+      <Movies movies={movies} showIndividualMovie={showIndividualMovie}/>
     </div>
   )
 }
