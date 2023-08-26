a7f834cb345ddcc209852ee520006cf73020d9a9
Adrian Zabolitzki <adrianzab@gmail.com>
2023-08-26 11:46:06 -0600

    Add header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <header>
+      <h1 className="header-title">RANCID TOMATILLOS</h1>
+      <h2>BACK</h2>
+    </header>
+  )
+}
+
+
+export default Header
