df444fd42cdd779ab4229ad74ac01146640a6e7c
Marisa Wyatt <risawyatt22@gmail.com>
2023-08-27 15:16:19 -0600

    Add PropTypes to MovieCard

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,10 +1,18 @@
+import PropTypes from "prop-types";
 import "./MovieCard.css";
 
 function MovieCard({ img, id, showIndividualMovie }) {
   return (
-    <div className="movie-card" onClick={()=> showIndividualMovie(id)}><img className="movie-img" src={img} />
+    <div className="movie-card" onClick={() => showIndividualMovie(id)}>
+      <img className="movie-img" src={img} />
     </div>
-  )
+  );
 }
 
-export default MovieCard
+MovieCard.propTypes = {
+  img: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  showIndividualMovie: PropTypes.func.isRequired,
+};
+
+export default MovieCard;
