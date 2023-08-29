2030bec66e0ee6df258d668ea6c23fe18ed8e735
Adrian Zabolitzki <adrianzab@gmail.com>
2023-08-28 21:29:06 -0600

    Add cypress tests for single movie details

---
+++ cypress/e2e/selectedmoviepage_spec.cy.js
@@ -0,0 +1,24 @@
+describe('Rancid Tomatillos single movie selection', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    .intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+      statusCode: 201,
+      fixture: "moviesData.json"
+    })
+    .intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270", {
+      statusCode: 201,
+      fixture: "singleMovieData.json"
+    })
+    
+  });
+
+  // As a user, when I click on a movie, I’m shown additional details about that movie
+
+  it('should show movie details when a movie is selected', () => {
+    cy.get('.movie-card').find('img')
+    cy.get('.movie-card').first().click()
+    cy.get('.single-movie-cont').contains('h2', 'Black Adam')
+      .get('.single-movie-card').contains('h2', 'The world needed a hero. It got Black Adam.')
+      .get('.single-movie-card').contains('h2', 'RATING: 4.0 / 10')
+  })
+})
