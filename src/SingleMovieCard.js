8549cbff02fa890179a965d78b763858450e6f41
Marisa Wyatt <risawyatt22@gmail.com>
2023-08-31 17:07:52 -0600

    Remove unnecessary id prop

--- src/SingleMovieCard.js
+++ src/SingleMovieCard.js
@@ -1,7 +1,16 @@
-import PropTypes from 'prop-types'
-import "./SingleMovieCard.css"
+import PropTypes from "prop-types";
+import "./SingleMovieCard.css";
 
-function SingleMovieCard ({title, backdrop_path, id, average_rating, release_date, overview, tagline, runtime, genres}) {
+function SingleMovieCard({
+  title,
+  backdrop_path,
+  average_rating,
+  release_date,
+  overview,
+  tagline,
+  runtime,
+  genres,
+}) {
   return (
     <div className="single-movie-card">
       <h2 className="single-movie-title">{title}</h2>
@@ -10,32 +19,45 @@ function SingleMovieCard ({title, backdrop_path, id, average_rating, release_dat
       <h2>{overview}</h2>
       <section className="details-cont">
         <article className="details-article">
-          <h2>RATING:<br></br><br></br> {average_rating.toFixed(1)} / 10</h2>
+          <h2>
+            RATING:<br></br>
+            <br></br> {average_rating.toFixed(1)} / 10
+          </h2>
         </article>
         <article className="details-article">
-          <h2>RUNTIME:<br></br><br></br> {runtime} MINUTES</h2>
+          <h2>
+            RUNTIME:<br></br>
+            <br></br> {runtime} MINUTES
+          </h2>
         </article>
         <article className="details-article">
-          <h2>RELEASE DATE:<br></br><br></br>{release_date}</h2>
+          <h2>
+            RELEASE DATE:<br></br>
+            <br></br>
+            {release_date}
+          </h2>
         </article>
         <article className="details-article">
-          <h2>GENRES:<br></br><br></br> {genres.join(' / ')}</h2>
+          <h2>
+            GENRES:<br></br>
+            <br></br> {genres.join(" / ")}
+          </h2>
         </article>
       </section>
     </div>
-  )
+  );
 }
 
 SingleMovieCard.propTypes = {
-  title: PropTypes.string.isRequired, 
-  backdrop_path: PropTypes.string.isRequired, 
-  id: PropTypes.number.isRequired, 
-  average_rating: PropTypes.number.isRequired, 
-  release_date: PropTypes.string.isRequired, 
-  overview: PropTypes.string.isRequired, 
-  tagline: PropTypes.string.isRequired, 
-  runtime: PropTypes.number.isRequired, 
-  genres: PropTypes.array.isRequired
-}
+  title: PropTypes.string.isRequired,
+  backdrop_path: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  average_rating: PropTypes.number.isRequired,
+  release_date: PropTypes.string.isRequired,
+  overview: PropTypes.string.isRequired,
+  tagline: PropTypes.string.isRequired,
+  runtime: PropTypes.number.isRequired,
+  genres: PropTypes.array.isRequired,
+};
 
-export default SingleMovieCard
+export default SingleMovieCard;
