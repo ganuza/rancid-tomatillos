dacd5e9a3cd721ccde96c89136a80bb83641584e
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-03 15:49:03 -0600

    Refactored error messaging

--- cypress/e2e/errorhandlingspec.cy.js
+++ cypress/e2e/errorhandlingspec.cy.js
@@ -1,12 +1,17 @@
 describe('Error handling', () => {
-  it('should display an error message when the server returns a 500 response on page load', () => {
+  beforeEach(() => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { 
       statusCode: 500
-    })
+    }).as('interceptAllMoviesFetch')
+  })
+
+  it('should display an error message when the server returns a 500 response on page load', () => {
     cy.visit('http://localhost:3000')
-    cy.get(ErrorMessages)
-    cy.get('.error-message').should('exist')
-    cy.get('.error-message').should('contain', "Request Failed: Something Went Wrong On The Server")
+    cy.wait('@interceptAllMoviesFetch')
+    cy.get('header').should('exist')
+    // cy.get('.p-movies-error').should('exist')
+    cy.get('.app').last()
+    cy.contains("Request Failed: Movies Page Something Went Wrong On The Server")
   })
   it('should display an error message when the server returns a 500 response for a single movie', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { 
@@ -17,7 +22,7 @@ describe('Error handling', () => {
     })
     cy.visit('http://localhost:3000/436270')
     cy.url().should('include', '/436270')
-    cy.get('.error-message').should('exist')
-    cy.get('.error-message').should('contain', "Request Failed: Something Went Wrong On The Server")
+    cy.get('.app').should('exist')
+    cy.get('.error-message').should('contain', "Request Failed: Movies Page Something Went Wrong On The Server")
   })
 })
